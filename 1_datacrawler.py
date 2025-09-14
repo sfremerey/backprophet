dea3f58e9cf068720baae4b2addc929ede36e7d1
Stephan Fremerey <stephan.fremerey@tu-ilmenau.de>
2025-09-14 18:15:25 +0200

    Add crawling of FEARANDGREED from CNN API.

--- 1_datacrawler.py
+++ 1_datacrawler.py
@@ -3,11 +3,16 @@ import matplotlib.pyplot as plt
 import pandas as pd
 # import pandas_datareader as pdr
 import seaborn as sns
-import urllib.request
 import yfinance as yf
+import requests, csv, json, urllib
+import time
+
+from fake_useragent import UserAgent
+from datetime import datetime
 
 
-YEARS_BACK = 3  # Number of years to crawl
+
+YEARS_BACK = 5  # Number of years to crawl, maximum about 5 as CNN API (Fear & Greed Index) does not go back further
 # List of symbols to crawl from yfinance in addition to S&P 500
 # For more details, cf. https://finance.yahoo.com/quote/[symbol]
 YFINANCE_SYMBOLS = ["^DJI", "META"]
@@ -45,6 +50,33 @@ def get_append_gprd(df):
     df_return = df.merge(df_gprd[["DATE", "GPRD"]], on="DATE", how="left")
     return df_return
 
+def get_append_fearandgreed(df, start_date):
+    # Crawls Fear and Greed Index from CNN API
+    # Also cf. https://edition.cnn.com/markets/fear-and-greed
+    # Inspired by https://medium.com/@polish.greg/fear-and-greed-index-python-scraper-96e71e57dbd0
+    fearandgreed_base_url = "https://production.dataviz.cnn.io/index/fearandgreed/graphdata/"
+    ua = UserAgent()
+    headers = {
+        "User-Agent": ua.random  # Use random user-agent to prevent API errors
+    }
+    r = requests.get(f"{fearandgreed_base_url}{str(start_date)}", headers=headers)
+    if r.status_code == 200:
+        data = r.json()["fear_and_greed_historical"]["data"]
+        df_fearandgreed = pd.DataFrame(data)
+        df_fearandgreed = df_fearandgreed.rename(columns={
+            "x": "DATE",
+            "y": "FEARANDGREED"
+        })
+        df_fearandgreed["DATE"] = pd.to_datetime(df_fearandgreed["DATE"].astype(int), unit="ms", utc=True).dt.date
+        df_return = df.merge(df_fearandgreed[["DATE", "FEARANDGREED"]], on="DATE", how="left")
+        return df_return
+    elif r.status_code == 500:
+        print("Server error 500: internal error at the API. Please choose a smaller YEARS_BACK parameter to add Fear & Greed Index data...")
+        return df
+    else:
+        print(f"Unexpected status {r.status_code} from the CNN API...")
+        return df
+
 def main():
     start_date = pd.Timestamp.today() - pd.DateOffset(years=YEARS_BACK)
     start_date = pd.to_datetime(start_date).date()
@@ -76,20 +108,26 @@ def main():
         df = get_append_close_volume_from_yfinance(df, symbol, start_date, end_date)
 
     # Append data from geopolitical risk (GPR) index from https://www.matteoiacoviello.com/gpr.htm
-    # The daily data are updated every Monday.
+    # The daily data (GPRD) are updated every Monday.
     # If the first day of the month or week falls on a federal holiday, data updates will take place the next business day.
+    print(f"Crawl data from matteoiacoviello.com for GPRD...")
     df = get_append_gprd(df)
+
+    # Append Fear&Greed data to df
+    print(f"Crawl data from CNN API for Fear & Greed Index...")
+    df = get_append_fearandgreed(df, start_date)
+
+    # Imputing, dropping NaN values, save df
     print("\n df before imputing and dropping NaN values:")
     df.info()
     df = df.ffill()  # Impute NA/NaN values by propagating the last valid observation to next valid.
     df = df.dropna()  # Drop all rows with NA/NaN values, could e.g. happen when some of the FRED values are not there for the very 1st row
     print("\n df after imputing and dropping NaN values:")
     df.info()
+    # Optional: scale data - not done yet - maybe todo
     print(f"\n Save data to data/{end_date}.csv ...")
     df.to_csv(f"data/{end_date}.csv",index=False)
 
-    # Append Fear&Greed data to df
-    # To be implemented, cf. https://medium.com/@polish.greg/fear-and-greed-index-python-scraper-96e71e57dbd0
 
 if __name__ == "__main__":
     main()

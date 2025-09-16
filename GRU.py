927a9fe66b8580ba40c2525c90c19abd4ca4b6c2
ivogl <ivo.glueck@gmail.com>
2025-09-16 14:39:25 +0200

    First GRU trial

---
+++ GRU.py
@@ -0,0 +1,9 @@
+import torch
+import torch.nn as nn
+
+rnn = nn.GRU(10, 20, 2)
+input = torch.randn(5, 3, 10)
+h0 = torch.randn(2, 3, 20)
+output, hn = rnn(input, h0)
+
+class torch.nn.GRU(input_size, hidden_size, num_layers=1, bias=True, batch_first=False, dropout=0.0, bidirectional=False, device=None, dtype=None)

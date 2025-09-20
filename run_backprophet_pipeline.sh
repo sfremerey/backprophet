7316799e900ac4e3a1397632b511495527db990e
Stephan Fremerey <stephan.fremerey@googlemail.com>
2025-09-20 17:40:29 +0200

    Mod .sh pipeline script.

--- run_backprophet_pipeline.sh
+++ run_backprophet_pipeline.sh
@@ -1,15 +1,15 @@
-#/bin/fish
+#!/usr/bin/env bash
 cd /home/sfremerey/backprophet
 git pull
-source .venv/bin/activate.fish
-python 1_datacrawler.py
-python 2_simple_mlp.py
-python 3_lstm.py
-python 4_gru.py
-python 5_rnn.py
-python 6_cnn.py
-python 7_ensemble.py
-python 8_sentiment.py
+./.venv/bin/python 1_datacrawler.py
+export MKL_DEBUG_CPU_TYPE=5
+./.venv/bin/python 2_simple_mlp.py
+./.venv/bin/python 3_lstm.py
+./.venv/bin/python 4_gru.py
+./.venv/bin/python 5_rnn.py
+./.venv/bin/python 6_cnn.py
+./.venv/bin/python 7_ensemble.py
+./.venv/bin/python 8_sentiment.py
 git add data/META_predictions.csv
 git add data/META_sentiment.csv
 git commit -m "Add recent predictions and sentiment analysis."
